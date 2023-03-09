8a27eb6b353597ff373d9a720640a020684ed6a7
sevel <zhaobinng.liu@outlook.com>
2023-03-09 18:46:20 +0800

    1.add(新增asr音频文件下载功能)

--- apps/asr/src/download_wav.py
+++ apps/asr/src/download_wav.py
@@ -1,4 +1,6 @@
+# -*- coding:utf-8 -*-
 import requests
+import subprocess
 
 from utils.handler import Handlers
 
@@ -14,12 +16,12 @@ def get_question_ids(file_path, sheet_name):
 
 
 def big_data(question_ids):
-    url = "http://172.16.23.83:30515/roc/quoto/cdmCo"
+    url = "http://kubernetes-prod-3.cloudminds.com:30515/data/quoto/cdmCo"
     payload = {
         "[]": {
             "dwm_svo_anno_label_event_i_d": {
                 "question_id{}": question_ids,
-                "@column": "question_id,rcu_audio"
+                "@column": "question_id,rcu_audio,question_text"
             },
             "page": 0,
             "count": len(question_ids)
@@ -43,23 +45,19 @@ def download_file(url, file_name):
 
 
 if __name__ == '__main__':
-    question_ids_list, info = get_question_ids(file_path=r"C:\Users\liuzhaobing\Desktop\asr.xlsx", sheet_name="Sheet2")
+    question_ids_list, info = get_question_ids(file_path=r"./data/asr自动化测试-李森.xlsx", sheet_name="原始数据")
     results = big_data(question_ids=question_ids_list)
-    path = r"D:\\workspace\\"
+    path = ""
     test_cases = []
+
     for case in results:
         question_id = case["dwm_svo_anno_label_event_i_d"]["question_id"]
         rcu_audio = case["dwm_svo_anno_label_event_i_d"]["rcu_audio"]
         text = info[question_id]
         filename = path + text + "__" + question_id + ".wav"
-        try:
-            download_file(url=rcu_audio, file_name=filename)
-            test_cases.append({
-                "question_text": text,
-                "wav_filename": filename,
-                "question_id": question_id
-            })
-        except Exception as e:
-            print(e)
-            continue
+        rcu_audio.replace("&", "\&")
+        result = subprocess.run(f"wget {rcu_audio} -O {filename}", shell=True, capture_output=True, text=True)
+        error = result.stderr
+        output = result.stdout
+
     Handlers.write_list_map_as_excel(test_cases, excel_writer=f"{path}asr_cases.xlsx", sheet_name="Sheet1", index=False)
